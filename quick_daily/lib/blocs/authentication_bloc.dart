a2bd71df4fe4570d13f2cd2387083fb52d771cf1
Marcin Kazmierski <m.kazmierski@expans.io>
2020-02-26 10:37:35 +0100

    Auth with BloC - states and events.

---
+++ quick_daily/lib/blocs/authentication_bloc.dart
@@ -0,0 +1,89 @@
+import 'package:equatable/equatable.dart';
+import 'package:bloc/bloc.dart';
+import 'dart:async';
+import 'package:quick_daily/repositories/api_repository.dart';
+
+abstract class AuthenticationState extends Equatable {
+  @override
+  List<Object> get props => [];
+}
+
+// uninitialized - waiting to see if the user is authenticated or not on app start.
+// loading - waiting to persist/delete a token
+// authenticated - successfully authenticated
+// unauthenticated - not authenticated
+
+// if the authentication state was uninitialized, the user might be seeing a splash screen.
+// if the authentication state was loading, the user might be seeing a progress indicator.
+// if the authentication state was authenticated, the user might see a home screen.
+// if the authentication state was unauthenticated, the user might see a login form.
+
+/// STATES
+class AuthenticationUninitialized extends AuthenticationState {}
+
+class AuthenticationAuthenticated extends AuthenticationState {}
+
+class AuthenticationUnauthenticated extends AuthenticationState {}
+
+class AuthenticationLoading extends AuthenticationState {}
+
+/// EVENTS
+abstract class AuthenticationEvent extends Equatable {
+  const AuthenticationEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class AppStarted extends AuthenticationEvent {}
+
+class LoggedIn extends AuthenticationEvent {
+  final String token;
+
+  const LoggedIn({this.token});
+
+  @override
+  List<Object> get props => [token];
+
+  @override
+  String toString() => 'LoggedIn { token: $token }';
+}
+
+class LoggedOut extends AuthenticationEvent {}
+
+/// BLOC
+class AuthenticationBloc
+    extends Bloc<AuthenticationEvent, AuthenticationState> {
+  final ApiRepository apiRepository;
+
+  AuthenticationBloc({this.apiRepository}) : assert(apiRepository != null);
+
+  @override
+  AuthenticationState get initialState => AuthenticationUninitialized();
+
+  @override
+  Stream<AuthenticationState> mapEventToState(
+      AuthenticationEvent event) async* {
+    if (event is AppStarted) {
+      final bool hasToken = await apiRepository.hasToken();
+
+      if (hasToken) {
+        yield AuthenticationAuthenticated();
+      } else {
+        yield AuthenticationUnauthenticated();
+      }
+    }
+
+    if (event is LoggedIn) {
+      yield AuthenticationLoading();
+      await apiRepository.persistToken(event.token);
+      yield AuthenticationAuthenticated();
+    }
+
+    if (event is LoggedOut) {
+      yield AuthenticationLoading();
+      await apiRepository.deleteToken();
+      yield AuthenticationUnauthenticated();
+    }
+  }
+}
