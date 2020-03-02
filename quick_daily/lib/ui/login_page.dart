4a66ee86bd7462b91155189ba619fbf4e2b8209c
Marcin Kazmierski <m.kazmierski@expans.io>
2020-03-02 11:29:44 +0100

    Login page.

--- quick_daily/lib/ui/login_page.dart
+++ quick_daily/lib/ui/login_page.dart
@@ -16,9 +16,6 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
       body: BlocProvider(
         create: (context) {
           return LoginBloc(
@@ -40,6 +37,7 @@ class LoginForm extends StatefulWidget {
 class _LoginFormState extends State<LoginForm> {
   final _usernameController = TextEditingController();
   final _passwordController = TextEditingController();
+  TextStyle _style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
 
   @override
   Widget build(BuildContext context) {
@@ -65,27 +63,96 @@ class _LoginFormState extends State<LoginForm> {
       },
       child: BlocBuilder<LoginBloc, LoginState>(
         builder: (context, state) {
-          return Form(
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'username'),
-                  controller: _usernameController,
-                ),
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'password'),
-                  controller: _passwordController,
-                  obscureText: true,
-                ),
-                RaisedButton(
-                  onPressed:
-                      state is! LoginLoading ? _onLoginButtonPressed : null,
-                  child: Text('Login'),
-                ),
+          return Scaffold(
+            body: Stack(
+              children: <Widget>[
                 Container(
-                  child: state is LoginLoading
-                      ? CircularProgressIndicator()
-                      : null,
+                  decoration: new BoxDecoration(
+                      image: new DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage("assets/images/background.jpg"))),
+                ),
+                Center(
+                  child: SingleChildScrollView(
+                    padding: EdgeInsets.all(25.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        CircleAvatar(
+                          radius: 58.0,
+                          backgroundColor: Colors.teal,
+                          foregroundColor: Colors.white,
+                          child: Icon(Icons.phone_in_talk, size: 50),
+                        ),
+                        SizedBox(
+                          height: 30.0,
+                        ),
+                        TextFormField(
+                          controller: _usernameController,
+                          decoration: InputDecoration(
+                              prefixIcon: Icon(
+                                Icons.person,
+                                color: Colors.white,
+                              ),
+                              hintStyle: TextStyle(color: Colors.white),
+                              filled: true,
+                              fillColor: Colors.black45,
+                              hintText: 'Username'),
+                        ),
+                        SizedBox(
+                          height: 10.0,
+                        ),
+                        TextFormField(
+                          controller: _passwordController,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                              filled: true,
+                              prefixIcon: Icon(Icons.lock, color: Colors.white),
+                              hintStyle: TextStyle(color: Colors.white),
+                              fillColor: Colors.black45,
+                              hintText: 'Password'),
+                        ),
+                        SizedBox(
+                          height: 15.0,
+                        ),
+                        FlatButton(
+                            onPressed: () {},
+                            child: Text(
+                              'Forgot your Password?',
+                              style: TextStyle(color: Colors.white),
+                            )),
+                        SizedBox(
+                          height: 15.0,
+                        ),
+                        Center(
+                          child: state is LoginLoading
+                              ? CircularProgressIndicator()
+                              : null,
+                        ),
+                        RaisedButton(
+                          onPressed: state is! LoginLoading
+                              ? _onLoginButtonPressed
+                              : null,
+                          child: Padding(
+                              padding: EdgeInsets.all(15.0),
+                              child: Text('LOGIN')),
+                          color: Colors.teal,
+                          textColor: Colors.white,
+                        ),
+                        SizedBox(
+                          height: 10.0,
+                        ),
+                        RaisedButton(
+                          onPressed: () {},
+                          child: Padding(
+                              padding: EdgeInsets.all(15.0),
+                              child: Text('REGISTER')),
+                          color: Colors.grey,
+                          textColor: Colors.white,
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ],
             ),
