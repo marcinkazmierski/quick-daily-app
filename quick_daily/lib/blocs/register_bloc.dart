2d441875c54722e7778672f4e7f699e02d9cea0a
Marcin Kazmierski <m.kazmierski@expans.io>
2020-03-28 09:47:27 +0100

    RegisterBloc events

--- quick_daily/lib/blocs/register_bloc.dart
+++ quick_daily/lib/blocs/register_bloc.dart
@@ -12,9 +12,48 @@ abstract class RegisterState extends Equatable {
 
 class RegisterInitial extends RegisterState {}
 
+class RegisterFailure extends RegisterState {
+  final String error;
+
+  const RegisterFailure({this.error});
+
+  @override
+  List<Object> get props => [error];
+
+  @override
+  String toString() => 'RegisterFailure { error: $error }';
+}
+
 /// EVENTS
 abstract class RegisterEvent extends Equatable {
   const RegisterEvent();
+}
+
+class RegisterButtonPressed extends RegisterEvent {
+  @override
+  List<Object> get props => [];
+
+  @override
+  String toString() => 'RegisterButtonPressed {}';
+}
+
+class RegisterSubmit extends RegisterEvent {
+  final String nick;
+  final String email;
+  final String password;
+
+  const RegisterSubmit({
+    this.nick,
+    this.email,
+    this.password,
+  });
+
+  @override
+  List<Object> get props => [nick, email, password];
+
+  @override
+  String toString() =>
+      'RegisterSubmit { nick: $nick, email: $email, password: $password }';
 }
 
 /// BLOC
