bedf51c1ac57a4145e1cb054d6551e9a6ccd5194
Marcin Kazmierski <m.kazmierski@expans.io>
2020-04-16 15:11:58 +0200

    firebase_crashlytics.

--- quick_daily/lib/main.dart
+++ quick_daily/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:bloc/bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -8,6 +9,7 @@ import 'package:quick_daily/ui/register_page.dart';
 import 'package:quick_daily/ui/splash_page.dart';
 import 'package:quick_daily/common/loading_indicator.dart';
 import 'package:quick_daily/ui/teams_page.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 class SimpleBlocDelegate extends BlocDelegate {
   @override
@@ -30,19 +32,30 @@ class SimpleBlocDelegate extends BlocDelegate {
 }
 
 void main() {
+  // Set `enableInDevMode` to true to see reports while in debug mode
+  // This is only to be used for confirming that reports are being
+  // submitted as expected. It is not intended to be used for everyday
+  // development.
+  Crashlytics.instance.enableInDevMode = true;
+  // Pass all uncaught errors to Crashlytics.
+  FlutterError.onError = Crashlytics.instance.recordFlutterError;
+
   BlocSupervisor.delegate = SimpleBlocDelegate();
   final apiRepository = ApiRepository();
-  runApp(
-    BlocProvider<AuthenticationBloc>(
-      create: (context) {
-        AuthenticationBloc auth =
-            AuthenticationBloc(apiRepository: apiRepository);
-        auth.add(AppStarted());
-        return auth;
-      },
-      child: App(apiRepository: apiRepository),
-    ),
-  );
+
+  runZoned(() {
+    runApp(
+      BlocProvider<AuthenticationBloc>(
+        create: (context) {
+          AuthenticationBloc auth =
+              AuthenticationBloc(apiRepository: apiRepository);
+          auth.add(AppStarted());
+          return auth;
+        },
+        child: App(apiRepository: apiRepository),
+      ),
+    );
+  }, onError: Crashlytics.instance.recordError);
 }
 
 class App extends StatelessWidget {
